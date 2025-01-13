0835de285c83b48283d68b4f4f69a4a7a07b0cf2
FilippoSalterini <s4516129@studenti.unige.it>
2025-01-13 10:20:26 +0100

    Update distance.cpp exam rt

--- src/distance.cpp
+++ src/distance.cpp
@@ -3,6 +3,10 @@
 #include "geometry_msgs/Twist.h"
 #include "turtlesim/Spawn.h"
 #include "std_msgs/Float32.h"      //needed for publishing the distances
+
+#include "sensor_msgs/LaserScan.h"   //for Exam1RT, it read from a sensor that returns informations about the environment, topic /obstacles
+#include <cmath>
+
 #include <iostream>
 #include <cmath>
 #include <string>
@@ -15,6 +19,9 @@ turtlesim::Pose turtle2_position;
 // i define 2 publisher for the velocity of the 2 turtle
 ros::Publisher vel_turtle_pub1;
 ros::Publisher vel_turtle_pub2;
+
+//examRT
+ros::Publisher distance_pub; //global publisher that publish the min distance 
 
 //now i have to callback the position for turtle1 and turtle2
 void Turtle1CallBackPosition (const turtlesim::Pose::ConstPtr& msg){
@@ -75,14 +82,14 @@ void TurtleWall() {
 }
 
 void TurtleImpact() {
-       const float THRESHOlD = 1.0; //here I define a constant distance value for the 2 turtles to be used in the function to avoid the collision between the two
+       const float TRESHOlD = 1.0; //here I define a constant distance value for the 2 turtles to be used in the function to avoid the collision between the two
        //i set a float distance that gaves me in realt time the distances between the 2 turtles
        float distance = TurtleDistance(turtle1_position, turtle2_position);
        //create 2 twist msg for velocity adjustment
        geometry_msgs::Twist vel_turtle1;
        geometry_msgs::Twist vel_turtle2;
        //here I check if the distance between the two turtles is less than the previously defined limit "TRESHOLD"
-       if(distance < THRESHOlD) {
+       if(distance < TRESHOlD) {
        ROS_WARN_STREAM("THE TURTLE ARE TOO CLOSE!: \n" << distance);
        // Move turtles apart by steering them in opposite directions
        if(turtle1_position.x > turtle2_position.x){
@@ -110,6 +117,35 @@ void TurtleImpact() {
        }
 }
 
+
+//For ExamRT 13/01
+//sensor on my turtle cover 360degrees
+//receive a msg from laser scan containing the info about the distance of the obstacles
+void AvoidObstacle(const sensor_msgs::LaserScan::ConstPtr& msg) {
+     
+     //if my min distance is less then my treshold i will stop the robot
+     const float treshold = 1;
+     float min_distance = numeric_limits<float>::infinity(); 
+     
+     //found the min distance in the laser scanner values
+     // range[] array di distanze dei vari punti rilevati dal laser
+     for( float range : msg->ranges) {
+     if (range < min_distance && range >= msg->range_min && range <= msg->range_max)
+         min_distance = range;
+         
+         if(min_distance < treshold) {
+         ROS_WARN_STREAM("ROBOT TOO MUCH NEAR TO THE OBSTACLE (STOP THE ROBOT)!: \n" << min_distance);
+         vel_turtle1.linear.x = 0;
+         vel_turtle1.linear.y = 0;
+         }
+     }
+     //here publish the min distance
+     std_msgs::Float32 distance_msg;
+     distance_msg.data = min_distance;
+     distance_pub.publish(distance_msg);
+}
+         
+
 int main(int argc, char **argv) {
 
 	ros::init(argc, argv, "distance_between_turtle_and_wall");
@@ -117,6 +153,12 @@ int main(int argc, char **argv) {
 	
         ros::Subscriber turtle1_sub = nh.subscribe("/turtle1/pose", 10, Turtle1CallBackPosition);
         ros::Subscriber turtle2_sub = nh.subscribe("/turtle2/pose", 10, Turtle2CallBackPosition);
+        
+        //examRT
+        //subscriber per i dati del laser che ritorna la distanza del robot dagli ostacoli, on topic /obstacles
+        ros::Subscriber laser_sub = nh.subscribe<sensor_msgs::LaserScan>("/obstacles" , 16, AvoidObstacle);
+        //publisher per la distanza minima
+        distance_pub = nh.advertise<std_msgs::Float32>("min_distance",10);
         
         vel_turtle_pub1 = nh.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 10);
         vel_turtle_pub2 = nh.advertise<geometry_msgs::Twist>("/turtle2/cmd_vel", 10);
@@ -149,6 +191,7 @@ int main(int argc, char **argv) {
         
         TurtleImpact();     //call the impact function to check if they are too close
         TurtleWall();       //function that permitt the turtles to avoid the walls
+        AvoidObstacle();
         
         ros::spinOnce();    //here process the turtle callback, without any tipe of blocking loops
         loop_rate.sleep();
